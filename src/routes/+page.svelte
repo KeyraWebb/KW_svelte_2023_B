a2244221d53063b88dbe80590a43910eaeb4ea95
Keyra Webb (1000119056) <WEBBKJ1@student.op.ac.nz>
2023-07-26 09:06:50 +1200

    messed around with html - hw week 2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>UwU OwO</h1>
-<p>o no</p>
+<h2>HTML practice</h2>
+
+<ul>
+    <li>cat</li>
+    <li>dog</li>
+    <li>fish</li>
+    <li>bird</li>
+</ul>
+
+<img src = "https://ichef.bbci.co.uk/news/1024/cpsprodpb/4B11/production/_107971291_nessie.jpg" width="1000" alt = "A picture of the Loch Ness Monster"/> <!-- "Dinosaur" -->
+
+<div>
+    <a href = "https://www.op.ac.nz/" target = "_blank">Otago Polytechnic</a>
+</div>
