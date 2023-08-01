bd3d4df6b41cc743c16b1b60da38d51e86a89992
Keyra Webb (1000119056) <WEBBKJ1@student.op.ac.nz>
2023-08-02 09:25:14 +1200

    added a Nav component to be used in the header

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,3 +1,10 @@
+<script>
+  import Nav from "$lib/Nav.svelte";
+</script>
+
 <header>
     Hello, I am the Header
+    <div>
+        <Nav/>
+    </div>
 </header>
---
+++ src/lib/Nav.svelte
@@ -0,0 +1,2 @@
+<a href="/">Main page</a>
+<a href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ" target = "_blank">Otago Polytechnic</a>
