0d420ff1a0d1c15da545e78288a987588686193a
Keyra Webb (1000119056) <WEBBKJ1@student.op.ac.nz>
2023-08-02 09:00:08 +1200

    Added another page plus some extra things

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,9 @@
+<script>
+    import Header from '$lib/Header.svelte';
+    import Footer from '$lib/Footer.svelte';
+</script>
+
+<Header/>
 <h2>HTML practice</h2>
 
 <ul>
@@ -7,8 +13,10 @@
     <li>bird</li>
 </ul>
 
-<img src = "https://ichef.bbci.co.uk/news/1024/cpsprodpb/4B11/production/_107971291_nessie.jpg" width="1000" alt = "A picture of the Loch Ness Monster"/> <!-- "Dinosaur" -->
+<a href="/secret"><img src = "https://ichef.bbci.co.uk/news/1024/cpsprodpb/4B11/production/_107971291_nessie.jpg" width="1000" alt = " Loch Ness Monster"/></a> <!-- "Dinosaur" -->
 
 <div>
-    <a href = "https://www.op.ac.nz/" target = "_blank">Otago Polytechnic</a>
+    <a href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ" target = "_blank">Otago Polytechnic</a>
 </div>
+
+<Footer/>
---
+++ src/routes/secret/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+    import Header from '$lib/Header.svelte';
+    import Footer from '$lib/Footer.svelte';
+</script>
+
+<Header/>
+
+<h1>
+    Congrats! you have found my super secret page!
+</h1>
+
+<div>
+    <img src = "img/face_creep.png" width = 500 alt = "Face in the middle of the woods, hunched over and staring at you."/>
+</div>
+
+<a href="/">return</a>
+
+<Footer/>
