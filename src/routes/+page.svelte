8ab758b3427ece044a1a38f4d2bcd1687420f858
Keyra Webb (1000119056) <WEBBKJ1@student.op.ac.nz>
2023-09-06 14:11:35 +1200

    more work on the info grid

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,6 +7,8 @@
 <div class ="infoGrid">
     <img src = "../img/City.jpg" alt = "A cityscape" class = "infoGridItems">
     <p class = "infoGridItems">Visit amazing towns with fascinating histories and culture. Including the capital, Shadowspell, the gem of the country.</p>
+    <p class = "infoGridItems"></p>
+    <img class = "infoGridItems" src = "../img/waterfall.jpg" alt = "waterfall in the middle of a forest">
 </div>       
 
 
