883e00776fd200e656657699fc4c433403581d78
Keyra Webb (1000119056) <WEBBKJ1@student.op.ac.nz>
2023-09-06 13:57:06 +1200

    began work on the rest of the front page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,8 @@
     </p>
 </div> 
 <div class ="infoGrid">
-
+    <img src = "../img/City.jpg" alt = "A cityscape" class = "infoGridItems">
+    <p class = "infoGridItems">Visit amazing towns with fascinating histories and culture. Including the capital, Shadowspell, the gem of the country.</p>
 </div>       
 
 
