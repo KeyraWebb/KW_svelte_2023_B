beb7e016d3455f2da8fb2fb0212cad807b8a13d8
Keyra Webb (1000119056) <WEBBKJ1@student.op.ac.nz>
2023-09-08 11:55:21 +1200

    started working on the responsiveness on the main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,14 +9,20 @@
     <div class = "infoGridRight">
         <h2>Stunning Settlements</h2>
         <p>Visit amazing towns with fascinating histories and culture. Including the capital, Shadowspell, the gem of the country.</p>
-        <a href = "/settlements/">See more about our fascinating communities</a>
+        <a href = "/settlements/">Learn more about our fascinating communities</a>
     </div>
     <div class = "infoGridLeft">
         <h2>Enjoy N'turé</h2>
         <p>Explore the endless gorgeous scenery of L'nturé's forests and mountains. The beautiful and untouched landscape is full of fantastic oppurtunites of new experiences and breathtaking views, offering an unrivaled atmosphere of peace and serenity.</p>
-        <a href = "/nature/">Learn more about our enthralling nature activities<a>
+        <a href = "/nature/">Learn more about our enthralling nature activities</a>
     </div>
     <img class = "infoGridRight" src = "../img/waterfall.jpg" alt = "waterfall in the middle of a forest">
+    <img class = "infoGridLeft" src = "../img/overgrown-castle.jpg" alt = "overgrown castle entrance">
+    <div class = "infoGridRight">
+        <h2>Discover History</h2>
+        <p>Take our specialised tours or have a look around at historical landmarks yourself to get a phenomenal hands on experience with uncovering our countries rich past, and see how it affects our bright present.</p>
+        <a href = "/culture/">Learn more about our incredible culture and history</a>
+    </div>
 </div>       
 
 
