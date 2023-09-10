a605f1144e320fc9382dcab8f40ccd60a78cc0ac
Keyra Webb (1000119056) <WEBBKJ1@student.op.ac.nz>
2023-09-11 10:23:11 +1200

    front page infoGrid is now appropriately responsive

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,8 @@
     </p>
 </div> 
 <div class ="infoGrid">
-    <img class = "infoGridLeft" src = "../img/City.jpg" alt = "A cityscape">
+    <!-- <img class = "infoGridLeft" src = "../img/City.jpg" alt = "A cityscape"> -->
+    <div class = "infoGridLeft infoGridImg medievilCity"></div>
     <div class = "infoGridRight">
         <h2>Stunning Settlements</h2>
         <p>Visit amazing towns with fascinating histories and culture. Including the capital, Shadowspell, the gem of the country.</p>
@@ -16,8 +17,10 @@
         <p>Explore the endless gorgeous scenery of L'nturé's forests and mountains. The beautiful and untouched landscape is full of fantastic oppurtunites of new experiences and breathtaking views, offering an unrivaled atmosphere of peace and serenity.</p>
         <a href = "/nature/">Learn more about our enthralling nature activities</a>
     </div>
-    <img class = "infoGridRight" src = "../img/waterfall.jpg" alt = "waterfall in the middle of a forest">
-    <img class = "infoGridLeft" src = "../img/overgrown-castle.jpg" alt = "overgrown castle entrance">
+    <div class = "infoGridRight infoGridImg waterfallPic"></div>
+    <div class = "infoGridLeft infoGridImg overgrownCastle"></div>
+    <!-- <img class = "infoGridRight" src = "../img/waterfall.jpg" alt = "waterfall in the middle of a forest">
+    <img class = "infoGridLeft" src = "../img/overgrown-castle.jpg" alt = "overgrown castle entrance"> -->
     <div class = "infoGridRight">
         <h2>Discover History</h2>
         <p>Take our specialised tours or have a look around at historical landmarks yourself to get a phenomenal hands on experience with uncovering our countries rich past, and see how it affects our bright present.</p>
