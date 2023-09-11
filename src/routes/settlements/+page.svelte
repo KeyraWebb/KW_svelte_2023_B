c5a8591835f91579dadb9d2fb02625064b246495
Keyra Webb (1000119056) <WEBBKJ1@student.op.ac.nz>
2023-09-11 13:05:54 +1200

    began to work on settlements page, set up the town cards

---
+++ src/routes/settlements/+page.svelte
@@ -0,0 +1,14 @@
+<div class = "townCards">
+    <div class = "shadowspellCard">
+        <h2 class = "darkCard">Shadowspell</h2>
+    </div>
+    <div class = "fairedgeCard">
+        <h2>Fairedge</h2>
+    </div>
+    <div class = "silverwaldCard">
+        <h2>Silverwald</h2>
+    </div>
+    <div class = "tranmereCard">
+        <h2 class = "darkCard">Tran'meré</h2>
+    </div>
+</div>
