31875381a37dcbfdd0eae1067df865efcab312f8
KeyraWebb <keyrajrwebb@gmail.com>
2023-09-13 17:15:05 +1200

    added more content to the settlements page to make the card links more functional

--- src/routes/settlements/+page.svelte
+++ src/routes/settlements/+page.svelte
@@ -18,17 +18,29 @@
 </div>
 <div class = "article">
     <h1>Settlements</h1>
-    <p>L'nturé has many fantastic settlements</p>
+    <p>L'nturé has many fantastic settlements, ripe for exploration and relaxation for the perfect holiday experience.<br><br>
+        With legendary hospitality, engaging tours and entertaining activities, you'll never run out of things to do. 
+    </p>
 </div>
 <div class = "article" id = "shadowspell">
     <h1>Shadowspell</h1>
+    <p>Shadowspell, the gem of the country, as well as the nation's capital, is a beautiful city with rich history and beautiful archetechture. 
+        Entertain yourself by exploring it's streets. Take something home from the famous marketplace in the central trapezoid.</p>
+    <h3>Where You'll Stay</h3>
+    <h3>What You'll Do</h3>
 </div>
 <div class = "article" id = "fairedge">
     <h1>Fairedge</h1>
+    <h3>Where You'll Stay</h3>
+    <h3>What You'll Do</h3>
 </div>
 <div class = "article" id = "silverwald">
     <h1>Silverwald</h1>
+    <h3>Where You'll Stay</h3>
+    <h3>What You'll Do</h3>
 </div>
 <div class = "article" id = "tranmere">
     <h1>Tran'meré</h1>
+    <h3>Where You'll Stay</h3>
+    <h3>What You'll Do</h3>
 </div>
