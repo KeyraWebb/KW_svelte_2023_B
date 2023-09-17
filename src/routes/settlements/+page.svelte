32d5662a86eef5951020dd9199af493c9407c128
Keyra Webb (1000119056) <WEBBKJ1@student.op.ac.nz>
2023-09-18 11:23:29 +1200

    configured the main article layout for the settlements page with placeholder images

--- src/routes/settlements/+page.svelte
+++ src/routes/settlements/+page.svelte
@@ -16,6 +16,7 @@
         <h2>Tran'meré</h2>
     </div>
 </div>
+
 <div class = "article">
     <h1>Settlements</h1>
     <p>L'nturé has many fantastic settlements, ripe for exploration and relaxation for the perfect holiday experience.<br><br>
@@ -24,34 +25,69 @@
 </div>
 <div class = "article" id = "shadowspell">
     <h1>Shadowspell</h1>
-    <p>Shadowspell, the gem of the country, as well as the nation's capital, is a beautiful city with rich history and beautiful archetechture. 
-        Entertain yourself by exploring it's streets. Take something home from the famous marketplace in the central trapezoid.</p>
-    <h3>Where You'll Stay</h3>
-    <h3>What You'll Do</h3>
+    <div class = "infoGrid">
+        <div class = "infoGridImg shadowspellCard"></div>
+        <div>
+            <p>Shadowspell, the gem of the country, as well as the nation's capital, is a beautiful city with rich history and beautiful archetechture. 
+            Entertain yourself by exploring it's streets. Take something home from the famous marketplace in the central trapezoid.</p>
+        </div>
+        <div>
+            <h3>Where You'll Stay</h3>
+        </div>
+        <div class = "infoGridImg placeholder"></div>
+        <div class = "infoGridImg placeholder"></div>
+        <div>
+            <h3>What You'll Do</h3>
+        </div>
+    </div>
 </div>
+
 <div class = "article" id = "fairedge">
     <h1>Fairedge</h1>
-    <h3>Where You'll Stay</h3>
-    <h3>What You'll Do</h3>
+    <div class = "infoGrid">
+        <div class = "infoGridImg fairedgeCard"></div>
+        <div></div>
+        <div>
+            <h3>Where You'll Stay</h3>
+        </div>
+        <div class = "infoGridImg placeholder"></div>
+        <div class = "infoGridImg placeholder"></div>
+        <div>
+            <h3>What You'll Do</h3>
+        </div>
+    </div>
 </div>
 <div class = "article" id = "silverwald">
     <h1>Silverwald</h1>
-    <h3>Where You'll Stay</h3>
-    <h3>What You'll Do</h3>
+    <div class ="infoGrid">
+        <div class = "infoGridImg silverwaldCard"></div>
+        <div>
+        </div>
+        <div>
+            <h3>Where You'll Stay</h3>
+        </div>
+        <div class = "infoGridImg placeholder"></div>
+        <div class = "infoGridImg placeholder"></div>
+        <div>
+            <h3>What You'll Do</h3>
+        </div>
+    </div>
+
+
 </div>
 <div class = "article" id = "tranmere">
     <h1>Tran'meré</h1>
     <div class = "infoGrid">
-        <div class = "infoGridLeft"></div>
-        <div class = "infoGridRight">
-
+        <div class = "infoGridImg tranmereCard"></div>
+        <div>
+            
         </div>
-        <div class = "infoGridLeft">
+        <div>
             <h3>Where You'll Stay</h3>
         </div>
-        <div class = "infoGridRight"></div>
-        <div class = "infoGridLeft"></div>
-        <div class = "infoGridRight">
+        <div class = "infoGridImg placeholder"></div>
+        <div class = "infoGridImg placeholder"></div>
+        <div>
             <h3>What You'll Do</h3>
         </div>
     </div>
