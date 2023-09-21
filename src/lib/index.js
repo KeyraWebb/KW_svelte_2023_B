5bc13e6954bf5e2657b55243ea21f6e53aea87f0
Keyra Webb (1000119056) <WEBBKJ1@student.op.ac.nz>
2023-09-22 11:34:10 +1200

    creating a gallery using unsplash api

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,4 +1,5 @@
 let dropdownNav = function(){
-    var dropdown = document.getElementById("hiddenLinks");
+    let dropdown = document.getElementById("hiddenLinks");
     dropdown.style.height = dropdown.style.height === "105px" ? "0px" : "105px";
 }
+
--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -1,16 +1,15 @@
 <script>
   import { onMount } from "svelte";
+  import Castle from "../../lib/components/Castle.svelte";
 
     let castles = [];
-
-    
 
     const BASE_URL = 'https://api.unsplash.com'
     const CLIENT_ID = 'yaxJAENJi6W_YyX_CUCizhfQ5qi-W9LAAa4KUnlI87c'
 
     onMount(() => {
-        fetch('${BASE_URL}/search/photos?query=castles&client_id=${CLIENT_ID}')
-    .then(res => res.json)
+        fetch(`${BASE_URL}/search/photos?query=castles&client_id=${CLIENT_ID}`)
+    .then(res => res.json())
     .then(data => {
         castles = data.results;
     })
@@ -21,7 +20,7 @@
 <div class = "gallery">
     
     {#each castles as castle}
-        <img url = '${castle.urls.regular}' alt = 'castle'>
+        <Castle url = {castle.urls.regular}/>
     {/each}
 
 </div>
