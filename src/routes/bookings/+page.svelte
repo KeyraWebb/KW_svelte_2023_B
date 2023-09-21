dae3da2a236cfcb5288e18a7a9d57219fe0a23fb
Keyra Webb (1000119056) <WEBBKJ1@student.op.ac.nz>
2023-09-22 11:12:07 +1200

    began work on the booking form

---
+++ src/routes/bookings/+page.svelte
@@ -0,0 +1,15 @@
+<h1>Booking Form</h1>
+
+<form>
+    <label for = "fname">First Name: </label>
+    <input type="text" id = "fname"><br>
+    <label for = "lname">Last Name: </label>
+    <input type = "text" id = "lname"><br>
+    <label for = "package">Select your travel package: </label>
+    <input list = "packages" name = "package" id = "package">
+    <datalist id = "packages">
+        <option value = "Shadowspell"></option>
+        <option value = "Fairedge"></option>
+        <option value = "Silverwald"></option>
+    </datalist>
+</form>
