5cbfa8dcf27c848c768206acbee66e45f34765e9
Keyra Webb (1000119056) <WEBBKJ1@student.op.ac.nz>
2023-10-10 13:56:05 +1300

    added a home button to the nav

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,5 +1,6 @@
 <div class="nav">
     <div class = "hideLinks" id = "hiddenLinks">
+        <a href = "/" onclick="dropdownNav()">Home</a>
         <a href="/about/" onclick="dropdownNav()">About</a>
         <a href = "/settlements/" onclick="dropdownNav()">Settlements</a>
         <a href = "/nature/" onclick="dropdownNav()">Nature</a>
