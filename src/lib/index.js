216e1dbc13217da6b692ddedcf6b6b5f423f7fb1
KeyraWebb <keyrajrwebb@gmail.com>
2023-10-22 19:35:22 +1300

    changed the nav javascript to adjust based on window size

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,5 +1,11 @@
 let dropdownNav = function(){
     let dropdown = document.getElementById(`hiddenLinks`);
-    dropdown.style.height = dropdown.style.height === `105px` ? `0px` : `105px`;
+    if (window.innerWidth >= 1000) {
+        dropdown.style.height = dropdown.style.height === `105px` ? `0px` : `105px`;
+    }
+    else{
+        dropdown.style.height = dropdown.style.height === `735px` ? `0px` : `735px`;
+    }
+    
 }
 
