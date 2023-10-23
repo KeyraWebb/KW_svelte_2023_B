3daf6b899c21bc29b6f7518186e0c37929202927
Keyra Webb (1000119056) <WEBBKJ1@student.op.ac.nz>
2023-10-24 12:00:08 +1300

    added more styling to the booking form

--- src/routes/bookings/+page.svelte
+++ src/routes/bookings/+page.svelte
@@ -1,17 +1,19 @@
-<h1>Booking Form</h1>
+<div class=formSelect>
+    <h1>Booking Form</h1>
 
-<!--Booking form for people to 'sign up' for packages-->
+    <!--Booking form for people to 'sign up' for packages-->
 
-<form>
-    <label for = "fname">First Name: </label>
-    <input type="text" id = "fname"><br>
-    <label for = "lname">Last Name: </label>
-    <input type = "text" id = "lname"><br>
-    <label for = "package">Select your travel package: </label>
-    <input list = "packages" name = "package" id = "package">
-    <datalist id = "packages">
-        <option value = "Shadowspell"></option>
-        <option value = "Fairedge"></option>
-        <option value = "Silverwald"></option>
-    </datalist>
-</form>
+    <form>
+        <label for = "fname">First Name </label><br>
+        <input type="text" id = "fname" placeholder="Your name.."><br>
+        <label for = "lname">Last Name </label><br>
+        <input type = "text" id = "lname"  placeholder="Your Last name.."><br>
+        <label for = "package">Select your travel package </label><br>
+        <select id = "packages" name = "package">
+            <option value = "Blank"></option>
+            <option value = "Shadowspell"> Shadowspell </option>
+            <option value = "Fairedge"> Fairedge </option>
+            <option value = "Silverwald"> Silverwald </option>
+        </select>
+    </form>
+</div>
