bbd53a9b7f42fb3391d08f109a4eba8f6f87ebc4
KeyraWebb <keyrajrwebb@gmail.com>
2023-11-01 17:33:33 +1300

    created the article skeletons for the nature page

--- src/routes/nature/+page.svelte
+++ src/routes/nature/+page.svelte
@@ -2,24 +2,52 @@
 
 <div class = "townCards">
     <div>
-        <a href = "#top"><h2>Deepwick</h2></a>
-        <a href = "#top" class = "deepwickCard">&nbsp;</a>
+        <a href = "#deepwick"><h2>Deepwick</h2></a>
+        <a href = "#deepwick" class = "deepwickCard">&nbsp;</a>
     </div>
     <div>
-        <a href = "#top"><h2>Norhurst</h2></a>
-        <a href = "#top" class = "norhurstCard">&nbsp;</a>
+        <a href = "#norhurst"><h2>Norhurst</h2></a>
+        <a href = "#norhurst" class = "norhurstCard">&nbsp;</a>
     </div>
     <div>
-        <a href = "#top"><h2>Xin'daré</h2></a>
-        <a href = "#top" class = "xindareCard">&nbsp;</a>
+        <a href = "#xindare"><h2>Xin'daré</h2></a>
+        <a href = "#xindare" class = "xindareCard">&nbsp;</a>
     </div>
     <div>
-        <a href = "#top"><h2>L'naaré Mountains</h2></a>
-        <a href = "#top" class = "lnaareCard">&nbsp;</a>
+        <a href = "#lnaareMountains"><h2>L'naaré Mountains</h2></a>
+        <a href = "#lnaareMountains" class = "lnaareCard">&nbsp;</a>
     </div>
 </div>
 
 <div class = "article">
     <h1>Nature</h1>
     <div class = "infoGrid"></div>
+</div>
+
+<div class = "article" id = "deepwick">
+    <h1>Deepwick</h1>
+    <div class = "infoGrid">
+
+    </div>
+</div>
+
+<div class = "article" id = "norhurst">
+    <h1>Norhurst</h1>
+    <div class = "infoGrid">
+
+    </div>
+</div>
+
+<div class = "article" id = "xindare">
+    <h1>Xin'daré</h1>
+    <div class = "infoGrid">
+
+    </div>
+</div>
+
+<div class = "article" id = "lnaareMountains">
+    <h1>L'naaré Mountains</h1>
+    <div class = "infoGrid">
+
+    </div>
 </div>
