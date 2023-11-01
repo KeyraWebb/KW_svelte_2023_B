786364e49e290111637800c90cfd4e93ed5ab3f4
KeyraWebb <keyrajrwebb@gmail.com>
2023-11-01 17:27:35 +1300

    added the skeleton of the content for the culture page

---
+++ src/routes/culture/+page.svelte
@@ -0,0 +1,18 @@
+<div class = "article">
+    <h1>Culture</h1>
+    <div class = "infoGrid">
+        <div class = "infoGridImg placeholder"></div>
+        <div>
+            <p>L'nturé is bursting with Culture, Folklore and History, all ready for newcomers to discover and explore. </p>
+        </div>
+        <div>
+            <h2>Folklore</h2>
+            <p></p>
+        </div>
+        <div class = "infoGridImg placeholder"></div>
+        <div class = "infoGridImg placeholder"></div>
+        <div>
+            <h2>History</h2>
+        </div>
+    </div>
+</div>
