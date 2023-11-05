85fd8b9a726c4c650f1d72266d2e1768db1f93c3
Keyra Webb (1000119056) <WEBBKJ1@student.op.ac.nz>
2023-11-06 11:23:54 +1300

    all the placeholder images have been removed

--- src/routes/culture/+page.svelte
+++ src/routes/culture/+page.svelte
@@ -1,7 +1,7 @@
 <div class = "article">
     <h1>Culture</h1>
     <div class = "infoGrid">
-        <div class = "infoGridImg placeholder"></div>
+        <div class = "infoGridImg shadowspellSecondPic"></div>
         <div>
             <p>L'nturé is bursting with Culture, Folklore and History, all ready for newcomers to discover and explore. </p>
         </div>
@@ -9,8 +9,8 @@
             <h2>Folklore</h2>
             <p>L'nturé prides itself on our rich folklore, stemming from hundreds of years of vocal tradition.</p>
         </div>
-        <div class = "infoGridImg placeholder"></div>
-        <div class = "infoGridImg placeholder"></div>
+        <div class = "infoGridImg silverwaldThirdPic"></div>
+        <div class = "infoGridImg overgrownCastle"></div>
         <div>
             <h2>History</h2>
         </div>
